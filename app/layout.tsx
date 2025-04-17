6b54902202d9c3e17d707c63d6c491f61e8e8bae
Cezary Makowski <cezary.makowski96@gmail.com>
2025-04-17 20:54:00 +0200

    added GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Protest_Revolution } from "next/font/google";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -35,7 +35,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
-      {/* <GoogleAnalytics gaId="G-J59QCYVWFJ" /> */}
+      <GoogleAnalytics gaId="G-8WT96GVWTQ" />
       <GoogleTagManager gtmId="AW-16941751584" />
       <GoogleTagManager gtmId="GTM-WSS8JNJL" />
     </html>
